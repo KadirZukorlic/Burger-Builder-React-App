ac69c3e1f78c25556f7c4d19777c7366b8d8a7fa
Kadir Zurkolic <kadirzukorlic@gmail.com>
2020-12-25 00:20:21 +0100

    Mobile responsivness, toolbar, sidedrawer

--- Desktop/burger-basics--02-after-ingredients/src/components/Layout/Layout.js
+++ Desktop/burger-basics--02-after-ingredients/src/components/Layout/Layout.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+    SideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
 
-export default layout;
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+         return {showSideDrawer: !prevState.stateShowSideDrawer};
+        })
+    }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+        <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    closed={this.SideDrawerClosedHandler} />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+export default Layout;
