996b1b6949c51479ad3e63f0502b0e40544a4a54
Kadir Zurkolic <kadirzukorlic@gmail.com>
2021-01-02 01:26:49 +0100

    Form validation

--- Desktop/burger-basics--02-after-ingredients/src/containers/BurgerBuilder/Checkout/ContactData/ContactData.js
+++ Desktop/burger-basics--02-after-ingredients/src/containers/BurgerBuilder/Checkout/ContactData/ContactData.js
@@ -4,16 +4,63 @@ import Button from '../../../../components/UI/Button/Button';
 import Spinner from '../../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../../axios-orders';
+import Input from '../../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: '',
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name',
+        },
+        value: '',
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street',
+        },
+        value: '',
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'ZIP Code',
+        },
+        value: '',
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country',
+        },
+        value: '',
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your Mail',
+        },
+        value: '',
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+         options: [
+           {value: 'fastest', displayValue: 'Fastest'},
+           {value: 'cheapest', displayValue: 'Cheapest'}
+        ]
+        },
+        value: '',
+      },
     },
-    loading: false
+    loading: false,
   };
 
   orderHandler = (event) => {
@@ -22,16 +69,6 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      customer: {
-        name: 'Kadir Zukorlic',
-        address: {
-          street: 'Husein Bega Gradascevica BB',
-          zipCode: '36320',
-          country: 'Serbia',
-        },
-        email: 'kadirzukorlic@gmail.com',
-      },
-      deliveryMethod: 'fastest',
     };
     axios
       .post('/orders.json', order)
@@ -45,40 +82,30 @@ class ContactData extends Component {
   };
 
   render() {
-      let form = (
-        <form>
-        <input
-          className={classes.Input}
-          type="text"
-          name="name"
-          placeholder="Your Name"
-        ></input>
-        <input
-          className={classes.Input}
-          type="email"
-          name="email"
-          placeholder="Your Mail"
-        ></input>
-        <input
-          className={classes.Input}
-          type="text"
-          name="street"
-          placeholder="Street"
-        ></input>
-        <input
-          className={classes.Input}
-          type="text"
-          name="postal"
-          placeholder="Postal Code"
-        ></input>
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
+    let form = (
+      <form>
+        {formElementsArray.map(formElement => (
+          <Input 
+          key={formElement.id}
+          elementType={formElement.config.elementType}
+          elementConfig={formElement.config.elementConfig}
+          value={formElement.config.value}/>
+        ))}
         <Button btnType="Success" clicked={this.orderHandler}>
           ORDER
         </Button>
       </form>
-      );
-      if (this.state.loading) {
-          form = <Spinner />;
-      }
+    );
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
     return (
       <div className={classes.ContactData}>
         <h4>Enter your Contact Data</h4>
