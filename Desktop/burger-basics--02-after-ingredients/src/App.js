4ba18bded7a6b63f185c51b86a34b581b17e6ca4
Kadir Zurkolic <kadirzukorlic@gmail.com>
2021-01-15 00:36:19 +0100

    lazy loading

--- Desktop/burger-basics--02-after-ingredients/src/App.js
+++ Desktop/burger-basics--02-after-ingredients/src/App.js
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
 import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/BurgerBuilder/Checkout/Checkout';
-import Orders from './containers/BurgerBuilder/Orders/Orders';
-import Auth from './containers/BurgerBuilder/Auth/Auth';
 import Logout from './containers/BurgerBuilder/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
+
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/BurgerBuilder/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+  return import('./containers/BurgerBuilder/Orders/Orders');
+});
+
+const asyncAuth = asyncComponent(() => {
+  return import('./containers/BurgerBuilder/Auth/Auth');
+});
 
 class App extends Component {
   componentDidMount() {
@@ -17,7 +27,7 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-          <Route path="/auth" component={Auth} />
+          <Route path="/auth" component={asyncAuth} />
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to="/"/>
       </Switch>
@@ -26,10 +36,10 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes = (
       <Switch>
-         <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
+         <Route path="/checkout" component={asyncCheckout} />
+            <Route path="/orders" component={asyncOrders} />
             <Route path="/logout" component={Logout} />
-            <Route path="/auth" component={Auth} />
+            <Route path="/auth" component={asyncAuth} />
             <Route path="/" exact component={BurgerBuilder} />
             <Redirect to="/" />
       </Switch>
--- Desktop/burger-basics--02-after-ingredients/src/containers/BurgerBuilder/Auth/Auth.js
+++ Desktop/burger-basics--02-after-ingredients/src/containers/BurgerBuilder/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import {Redirect} from 'react-router-dom';
 
 import Input from '../../../components/UI/Input/Input';
@@ -7,7 +7,7 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './Auth.css';
 import * as actions from '../../../store/actions/index';
-
+import {updateObject, checkValidity } from '../../../shared/utility';
 
 class Auth extends Component {
   state = {
@@ -41,7 +41,7 @@ class Auth extends Component {
         touched: false,
       },
     },
-    isSignup: true
+    isSignup: true,
   };
 
   componentDidMount() {
@@ -50,60 +50,33 @@ class Auth extends Component {
     }
   }
 
-  checkValidity(value, rules) {
-    let isValid = true;
-    if (!rules) {
-        return true;
-    }
-    
-    if (rules.required) {
-        isValid = value.trim() !== '' && isValid;
-    }
+  
 
-    if (rules.minLength) {
-        isValid = value.length >= rules.minLength && isValid
-    }
-
-    if (rules.maxLength) {
-        isValid = value.length <= rules.maxLength && isValid
-    }
-
-    if (rules.isEmail) {
-        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-        isValid = pattern.test(value) && isValid
-    }
-
-    if (rules.isNumeric) {
-        const pattern = /^\d+$/;
-        isValid = pattern.test(value) && isValid
-    }
-
-    return isValid;
-}
-
-inputChangedHandler = (event, controlName) => {
-  const updatedControls = {
-    ...this.state.controls,
-    [controlName]: {
-      ...this.state.controls[controlName],
-      value: event.target.value,
-      valid: this.checkValidity(event.target.value, this.state.isValid),
-      touched: true
-    }
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = updateObject(this.state.controls, {
+      [controlName]: updateObject(this.state.controls[controlName], {
+        value: event.target.value,
+        valid: checkValidity(event.target.value, this.state.isValid),
+        touched: true,
+      })
+    } );
+    this.setState({controls: updatedControls});
   };
-  this.setState({controls: updatedControls});
-}
 
-submitHandler = (event) => {
-  event.preventDefault();
-  this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
-}
+  submitHandler = (event) => {
+    event.preventDefault();
+    this.props.onAuth(
+      this.state.controls.email.value,
+      this.state.controls.password.value,
+      this.state.isSignup
+    );
+  };
 
-switchAuthModeHandler = () => {
-  this.setState(prevState => {
-    return {isSignup: !prevState.isSignup};
-  })
-}
+  switchAuthModeHandler = () => {
+    this.setState((prevState) => {
+      return {isSignup: !prevState.isSignup};
+    });
+  };
 
   render() {
     const formElementsArray = [];
@@ -128,32 +101,30 @@ switchAuthModeHandler = () => {
     ));
 
     if (this.props.loading) {
-      form = <Spinner />
+      form = <Spinner />;
     }
 
     let errorMessage = null;
 
     if (this.props.error) {
-      errorMessage = (
-        <p>{this.props.error.message}</p>
-      )
+      errorMessage = <p>{this.props.error.message}</p>;
     }
     let authRedirect = null;
     if (this.props.isAuthenticated) {
-      authRedirect = <Redirect to={this.props.authRedirectPath}/>
-    }   
+      authRedirect = <Redirect to={this.props.authRedirectPath} />;
+    }
 
     return (
       <div className={classes.Auth}>
         {authRedirect}
         {errorMessage}
-        <form onSubmit={this.submitHandler}> 
+        <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
         </form>
-        <Button 
-        clicked={this.switchAuthModeHandler}
-        btnType="Danger">SWITCH TO {this.state.isSignup ? 'SIGNIN' : 'SIGNUP'}</Button>
+        <Button clicked={this.switchAuthModeHandler} btnType="Danger">
+          SWITCH TO {this.state.isSignup ? 'SIGNIN' : 'SIGNUP'}
+        </Button>
       </div>
     );
   }
@@ -165,15 +136,16 @@ const mapStateToProps = (state) => {
     error: state.auth.error,
     isAuthenticated: state.auth.token !== null,
     buildingBurger: state.burgerBuilder.building,
-    authRedirectPath: state.auth.authRedirectPath
+    authRedirectPath: state.auth.authRedirectPath,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
-    onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
-  }
-}
+    onAuth: (email, password, isSignup) =>
+      dispatch(actions.auth(email, password, isSignup)),
+    onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/')),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Auth);
