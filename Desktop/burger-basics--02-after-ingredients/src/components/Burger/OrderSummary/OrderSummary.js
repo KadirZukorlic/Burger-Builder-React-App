578842133e78364ab07cdb53f4da70530a58fc32
Kadir Zurkolic <kadirzukorlic@gmail.com>
2021-01-21 00:57:07 +0100

    added dependecies

--- Desktop/burger-basics--02-after-ingredients/src/components/Burger/OrderSummary/OrderSummary.js
+++ Desktop/burger-basics--02-after-ingredients/src/components/Burger/OrderSummary/OrderSummary.js
@@ -20,7 +20,7 @@ const orderSummary = props => {
             {ingredientSummary}
         </ul>
         <p>
-          <strong>Total Price: {props.price.toFixed(2)}</strong>
+          <strong>Total Price: {props.price}</strong>
         </p>
         <p>Continue to Checkout?</p>
         <Button btnType="Danger" clicked={props.purchaseCancelled}>
