6ba4c5f2d15ab5f783eed3e9ead2295c3f393ae3
Kadir Zurkolic <kadirzukorlic@gmail.com>
2021-04-02 00:12:24 +0200

    getting started, enabled sass, navbar

---
+++ Desktop/snews/src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+
+    return(
+        <nav className="nav">
+            <ul className="nav__list">
+                <li className="nav__list-item"></li>
+                <li className="nav__list-item"></li>
+                <li className="nav__list-item"></li>
+            </ul>
+        </nav>
+    );
+
+}
+
+export default Navbar;
