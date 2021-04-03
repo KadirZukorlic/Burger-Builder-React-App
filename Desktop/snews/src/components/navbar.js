93f3b7ffb2ffe9e28b1cb54e78dfa0c6bc8de057
Kadir Zurkolic <kadirzukorlic@gmail.com>
2021-04-03 22:55:29 +0200

    search input added

--- Desktop/snews/src/components/navbar.js
+++ Desktop/snews/src/components/navbar.js
@@ -1,21 +1,27 @@
 import React from 'react';
 
-import logo from '../assets/images/logo.png';
+// import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
   return (
-    <header className="header">
-      <img src={logo} alt="logo"/>
-      <nav className="nav">
+    <nav className="nav">
+      {/* <img src={logo} alt="logo" className="nav__logo"/> */}
+      
         <ul className="nav__list">
           <li className="nav__item"><a href="/politika" className="nav__link">Politika</a></li>
           <li className="nav__item"><a href="/vijesti" className="nav__link">Vijesti</a></li>
           <li className="nav__item"><a href="/komparacija" className="nav__link">Komparacija</a></li>
           <li className="nav__item"><a href="/kontakt" className="nav__link">Kontakt</a></li>
         </ul>
-      </nav>
-      <button class="btn-signup">Sign Up</button>
-    </header>
+        <div className="nav__search">
+          <input type="text" placeholder="Search"></input>
+          <button>Search</button>
+        </div>
+      <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <button className="nav__btn">Sign Up</button>
+        <button className="nav__btn">Sign In</button>
+      </div>
+    </nav>
   );
 };
 
