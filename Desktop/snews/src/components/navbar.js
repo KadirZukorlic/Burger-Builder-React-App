e41d13771afa5b0bc4d8f69900397158638d9d53
Kadir Zurkolic <kadirzukorlic@gmail.com>
2021-04-05 00:34:45 +0200

    adding button and links

--- Desktop/snews/src/components/navbar.js
+++ Desktop/snews/src/components/navbar.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import {Link, Router} from 'react-router-dom'
-import {Button} from './button';
-import Dropdown from './dropdown';
+import {Button} from './Button';
+import Dropdown from './Dropdown';
 
 // import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
-  const [click, setClick] = useState(false)
+  const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
 
   const clickHandler = () => setClick(!click)
-  console.log(click)
+  console.log(click, 'CLICK INITAL')
   const closeMobileMenu = () => setClick(false);
+  const dropdownHandler = () => setDropdown(!dropdown);
 
   return (
     <>
@@ -21,11 +23,37 @@ const Navbar = () => {
       </div>
       <ul className={click ? 'nav-menu active' : 'nav-menu'}>
        <li className="nav__item">
-         <Link to="/" className="nav__links" onClick={closeMobileMenu}>
+         <Link to="/pocetna" className="nav__links" onClick={closeMobileMenu}>
           Pocetna
          </Link>
        </li>
+
+       <li className="nav__item">
+         <Link to="/politika" className="nav__links" onClick={closeMobileMenu}>
+          Politika <i className="fas fa-caret-down"/>
+         </Link>
+         {dropdown && <Dropdown />}
+       </li>
+
+       <li className="nav__item">
+         <Link to="/komparacija" className="nav__links" onClick={closeMobileMenu}>
+          Komparacija
+         </Link>
+       </li>
+
+       <li className="nav__item">
+         <Link to="/contact-us" className="nav__links" onClick={closeMobileMenu}>
+          Kontaktiraj nas
+         </Link>
+       </li>
+
+       <li className="nav__item">
+         <Link to="/sign-up" className="nav__links-mobile" onClick={closeMobileMenu}>
+          Registruj se
+         </Link>
+       </li>
       </ul>
+      <Button />
     </nav>
     </>
   );
