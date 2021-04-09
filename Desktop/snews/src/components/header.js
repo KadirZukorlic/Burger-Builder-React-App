386f8a04bbbac1d18f37578004946dd26513f3a4
Kadir Zurkolic <kadirzukorlic@gmail.com>
2021-04-09 16:35:26 +0200

    working on header part

--- Desktop/snews/src/components/header.js
+++ Desktop/snews/src/components/header.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+import Logo from '../assets/images/logo.png';
 
 const Header = () => {
-    return (
-        <div className="header">
-            HEADER
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <img src={Logo} alt="Sandzak Logo" className="header__logo" />
+      <ul>
+        <li className="header__item">
+          <Link to="/sandzak" className="header__links">
+            Sandzak
+          </Link>
+        </li>
+
+        <li className="header__item">
+          <Link to="/dijaspora" className="header__links">
+            Dijaspora
+          </Link>
+        </li>
+
+        <li className="header__item">
+          <Link to="/sport" className="header__links">
+          Sport
+          </Link>
+        </li>
+
+        <li className="header__item">
+          <Link to="/ekonomija" className="header__links">
+            Ekonomija
+          </Link>
+        </li>
+
+        <li className="header__item">
+          <Link to="/tekstovi-nasih-autora" className="header__links">
+            Tekstovi nasih autora
+          </Link>
+        </li>
+
+        <li className="header__item">
+          <Link to="/intervjui" className="header__links">
+            Intervjui
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Header;
