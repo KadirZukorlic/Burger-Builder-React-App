939d99049bcff607798f1945110f88d756dafb69
Kadir Zurkolic <kadirzukorlic@gmail.com>
2021-04-14 18:01:04 +0200

    found soulution for putting text on image

--- Desktop/snews/src/components/Main.js
+++ Desktop/snews/src/components/Main.js
@@ -8,24 +8,26 @@ import main4 from '../assets/images/main-4.jpg';
 const Main = () => {
   return (
     <div className="main">
-        <figure class="main__item main__item--1">
-          <img src={main1} alt="Vijest-1" className="main__img" />
-          <h3>TEST</h3>
-          <p>13.april 2021</p>
-        </figure>
+      <figure className="main__item main__item--1">
+        <img src={main1} alt="Vijest-1" className="main__img" />
+        <div className="main__item-text--1">
+          <h1>TEST</h1>
+          <p>14. April 2021</p>
+        </div>
+      </figure>
 
-        <figure class="main__item main__item--2">
+      <figure className="main__item main__item--2">
         <img src={main2} alt="Vijest-2" className="main__img" />
-        </figure>
+      </figure>
 
-        <figure class="main__item main__item--3">
-          <img src={main3} alt="Vijest-3" className="main__img" />
-        </figure>
+      <figure className="main__item main__item--3">
+        <img src={main3} alt="Vijest-3" className="main__img" />
+      </figure>
 
-        <figure class="main__item main__item--4">
-          <img src={main4} alt="Vijest-4" className="main__img" />
-        </figure>
-      </div>
+      <figure className="main__item main__item--4">
+        <img src={main4} alt="Vijest-4" className="main__img" />
+      </figure>
+    </div>
   );
 };
 
