743d1f6b931db4d1ce72f5f880c0cd4183388323
Kadir Zurkolic <kadirzukorlic@gmail.com>
2021-04-19 16:32:22 +0200

    small bug fixes

--- Desktop/snews/src/components/Main.js
+++ Desktop/snews/src/components/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-import { Container } from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 
 import main1 from '../assets/images/main-1.jpg';
 import main2 from '../assets/images/main-2.jpg';
@@ -12,23 +13,43 @@ const Main = () => {
     <Container>
       <div className="main">
         <figure class="main__item main__item--1">
-          <img src={main1} alt="Vijest-1" className="main__img" />
-          <div className="main__item-text">
-            <h1> TEST TEST TEST TEST TEST TEST TEST TEST </h1>
-            <p>16. April 2021</p>
-          </div>
+          <Link to="/vijest-1">
+            <img src={main1} alt="Vijest-1" className="main__img" />
+            <div className="main__item-text">
+              <h1> TEST TEST TEST TEST TEST TEST TEST TEST </h1>
+              <p>16. April 2021</p>
+            </div>
+          </Link>
         </figure>
 
         <figure class="main__item main__item--2">
-          <img src={main2} alt="Vijest-2" className="main__img" />
+          <Link>
+            <img src={main2} alt="Vijest-2" className="main__img" />
+            <div className="main__item-text">
+              <h1> TEST TEST TEST TEST TEST TEST TEST TEST </h1>
+              <p>16. April 2021</p>
+            </div>
+          </Link>
         </figure>
 
         <figure class="main__item main__item--3">
-          <img src={main3} alt="Vijest-3" className="main__img" />
+          <Link>
+            <img src={main3} alt="Vijest-3" className="main__img" />
+            <div className="main__item-text">
+              <h1> TEST TEST TEST TEST TEST TEST TEST TEST </h1>
+              <p>16. April 2021</p>
+            </div>
+          </Link>
         </figure>
 
         <figure class="main__item main__item--4">
-          <img src={main4} alt="Vijest-4" className="main__img" />
+          <Link>
+            <img src={main4} alt="Vijest-4" className="main__img" />
+            <div className="main__item-text">
+              <h1> TEST TEST TEST TEST TEST TEST TEST TEST </h1>
+              <p>16. April 2021</p>
+            </div>
+          </Link>
         </figure>
       </div>
     </Container>
