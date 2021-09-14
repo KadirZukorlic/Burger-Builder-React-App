fa541fd96fe468b5785a9783e34f86727161385f
Kadir Zukorlic <kadirzukorlic@gmail.com>
2021-09-14 15:57:40 +0200

    ss

---
+++ Desktop/E-Commerce-App/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Header from "./components/Header";
+
+import "./default.scss";
+
+function App(props) {
+  return (
+    <div className="App">
+      <Header />
+      <div>
+        <h1>hello world</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/E-Commerce-App/src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Logo from "./../../assets/e-commerce-logo.png";
+
+import "../styles.scss";
+
+const Header = (props) => {
+  return (
+    <header className="header">
+      <div className="wrap">
+        <div className="logo">
+          <img src={Logo} alt="firm logo" />
+          <img src={Logo} alt="firm logo" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
